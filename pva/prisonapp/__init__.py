d7a2733c95e2d0779fc16b2bc6b4c5f700ab526e
Loyen Marie <loyenmarie.alegado@gmail.com>
2018-04-19 20:27:20 +0800

    Add files via upload

--- pva/prisonapp/__init__.py
+++ pva/prisonapp/__init__.py
@@ -10,10 +10,10 @@ from datetime import date
 
 
 app = Flask(__name__)
-
+server = Flask(__name__)
 
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:1234@localhost/prisonapp'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password271997@localhost/sample'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['USE_SESSION_FOR_NEXT'] = True
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -23,15 +23,13 @@ app.secret_key = os.urandom(24)
 db = SQLAlchemy(app)
 
 import prisonapp.api
-
+import prisonapp.server
 
 
 def createDB():
-    engine = sqlalchemy.create_engine('postgresql+psycopg2://postgres:1234@localhost') #connects to server
-    conn = engine.connect()
-    conn.execute("commit")
-    conn.execute("create database prisonapp")
-    conn.close()
+    engine = sqlalchemy.create_engine('mysql://root:password271997@localhost') #connects to server
+    engine.execute("CREATE DATABASE IF NOT EXISTS sample") #create db
+    engine.execute("USE sample") # select new
 
 def createTables():
     db.create_all()
