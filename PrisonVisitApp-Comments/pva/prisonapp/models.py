f9d3587a8657e4c5d80f6c281e6d971ec6213576
Leejames Merlas <beeboks34@gmail.com>
2018-04-24 17:01:59 +0800

    deleting files
    deleted leftover .idea files and added missing git ignore

--- PrisonVisitApp-Comments/pva/prisonapp/models.py
+++
@@ -1,9 +0,0 @@
-from prisonapp import *
-
-
-class Comments(db.Model):
-    __table__name = 'comments'
-    cid = db.Column(db.Integer(), primary_key=True)
-    vid = db.Column(db.Integer(), primary_key=True)
-    content = db.Column(db.String(60), nullable=False)
-    date = db.Column(db.DATE(), nullable=False)
