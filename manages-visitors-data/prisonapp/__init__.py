f0d6025e1f47e1ce1dab4f5a7a6b36b001345b56
LanceSanchez <31213346+LanceSanchez@users.noreply.github.com>
2018-04-25 05:43:30 +0800

    Done

---
+++ manages-visitors-data/prisonapp/__init__.py
@@ -0,0 +1,40 @@
+from flask import Flask, Blueprint, request, abort,url_for, jsonify, g, render_template, make_response,session
+from flask_sqlalchemy import SQLAlchemy
+import os, sqlalchemy, jwt, datetime
+from flask_httpauth import HTTPBasicAuth
+import uuid
+from werkzeug.security import generate_password_hash, check_password_hash
+from functools import wraps
+from flask_cors import CORS
+from datetime import date
+
+
+app = Flask(__name__)
+server = Flask(__name__)
+
+CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:sw33t890@localhost/prisonapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['USE_SESSION_FOR_NEXT'] = True
+app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['SECRET_KEY'] = 'thisissecret'
+app.secret_key = os.urandom(24)
+
+db = SQLAlchemy(app)
+
+import prisonapp.api
+import prisonapp.server
+
+
+def createDB():
+    engine = sqlalchemy.create_engine('postgresql+psycopg2://postgres:sw33t890@localhost') #connects to server
+    conn = engine.connect()
+    conn.execute("commit")
+    conn.execute("create database prisonapp")
+    conn.close()
+
+def createTables():
+    db.create_all()
+
+#createDB()
+createTables()
