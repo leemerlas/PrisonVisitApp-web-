0be430ff1595c47086c0fca5e9d243cf14beb623
Loyen Marie <loyenmarie.alegado@gmail.com>
2018-05-02 04:31:21 +0800

    visitation logs done

--- pva/prisonapp/api.py
+++ pva/prisonapp/api.py
@@ -1,6 +1,5 @@
 from prisonapp import *
-from models import User, Comment, Visitation
-
+from models import User, Comment, Visitation, VisitationLogs
 
 def token_required(f):
     @wraps(f)
@@ -11,12 +10,12 @@ def token_required(f):
             token = request.headers['x-access-token']
 
         if not token:
-            return jsonify({'message': 'token is missing!'}), 401
+            return jsonify ({'message':'token is missing!'}), 401
 
         try:
-            data = jwt.decode(token, app.config['SECRET_KEY'])
+            data=jwt.decode(token, app.config['SECRET_KEY'])
 
-            current_user = User.query.filter_by(public_id=data['public_id']).first()
+            current_user=User.query.filter_by(public_id=data['public_id']).first()
         except:
             return jsonify({'message': 'token is invalid!'}), 401
 
@@ -32,23 +31,20 @@ def register_user():
     data = request.get_json()
     hashed_password = generate_password_hash(data['password'], method='sha256')
 
-    new_user = User(public_id=str(uuid.uuid4()), username=data['username'], password_hash=hashed_password,
-                    firstname=data['firstname'], middlename=data['middlename'],
-                    lastname=data['lastname'], contact=data['contact'], address=data['address'],
-                    birthday=data['birthday'], prisoner=data['prisoner'], role_id=2, status=True,
+    new_user = User(public_id=str(uuid.uuid4()), username=data['username'], password_hash=hashed_password, firstname=data['firstname'], middlename=data['middlename'],
+                    lastname=data['lastname'], contact=data['contact'], address=data['address'], birthday=data['birthday'], prisoner=data['prisoner'], role_id=2, status=False,
                     age=data['age'])
     db.session.add(new_user)
     db.session.commit()
 
-    return jsonify({'message': 'Registered successfully!'})
-
+    return jsonify({'message':'Registered successfully!'})
 
 @app.route('/api/login/', methods=['GET'])
 def login():
     auth = request.authorization
 
     if not auth or not auth.username or not auth.password:
-        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm = "Login required!"'})
+        return make_response('Could not verify', 401, {'WWW-Authenticate':'Basic realm = "Login required!"'})
 
     user = User.query.filter_by(username=auth.username).first()
 
@@ -56,15 +52,10 @@ def login():
         return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm = "Login required!"'})
 
     if check_password_hash(user.password_hash, auth.password):
-        token = jwt.encode(
-            {'public_id': user.public_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},
-            app.config['SECRET_KEY'])
+        token = jwt.encode({'public_id':user.public_id, 'exp':datetime.datetime.utcnow()+datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
 
         print 'Token generated!'
-        return jsonify(
-            {'status': 'ok', 'token': token.decode('UTF-8'), 'role_id': user.role_id, 'public_id': user.public_id,
-             'message': 'login successful!'})
-
+        return jsonify({'status':'ok', 'token': token.decode('UTF-8'), 'role_id':user.role_id, 'public_id':user.public_id,'message':'login successful!'})
 
 @app.route('/api/user/visitation', methods=['POST'])
 @token_required
@@ -72,13 +63,11 @@ def visitation(current_user):
     data = request.get_json()
     user = User.query.filter_by(public_id=data['public_id']).first()
 
-    new_visit = Visitation(vId=int(user.id), nameP=data['nameP'], date=data['vDate'],
-                           numberOfVisitors=int(data['numV']), status='PENDING', time=data['timeV'],
-                           relationship=data['relation'])
+    new_visit = Visitation(vId=int(user.id),nameP=data['nameP'],date=data['vDate'],numberOfVisitors=int(data['numV']), status='PENDING')
     db.session.add(new_visit)
     db.session.commit()
 
-    return jsonify({'message': 'Success!'})
+    return jsonify({ 'message' : 'Success!' })
 
 
 @app.route('/api/user/comment', methods=['POST'])
@@ -91,13 +80,140 @@ def post_comment(current_user):
     db.session.add(new_comment)
     db.session.commit()
 
-    return jsonify({'message': 'Comment submitted! Thank you for your opinion!'})
+    return jsonify({'message':'Comment submitted! Thank you for your opinion!'})
 
 
-# END OF VISITOR API
+#END OF VISITOR API
 
 
-# START OF CLERK API
+#START OF CLERK API
+@app.route('/api/clerk/get_users', methods=['GET'])
+@token_required
+def get_all_users(current_user):
+
+    if current_user.role_id != '1':
+        return jsonify ({'message':'Cannot perform that function!'})
+
+    users = User.query.all()
+    output = []
+
+    for user in users:
+        user_data = {}
+        user_data['username'] = user.username
+        output.append(user_data)
+
+    return jsonify({ 'users':output })
+
+@app.route('/api/clerk/visitor_data', methods=['GET'])
+@token_required
+def get_visitors(current_user):
+    if current_user.role_id != '1':
+        return jsonify ({'message':'Cannot perform that function!'})
+
+    users = User.query.filter_by(role_id=2)
+
+    res = []
+
+    for user in users:
+        user_data = {}
+        user_data['firstname'] = user.firstname
+        user_data['middlename'] = user.middlename
+        user_data['lastname'] = user.lastname
+        user_data['age'] = user.age
+        user_data['contact'] = user.contact
+        user_data['address'] = user.address
+        user_data['birthday'] = user.birthday
+        user_data['status'] = user.status
+        user_data['id'] = user.id
+        res.append(user_data)
+
+    return jsonify({'status': 'ok', 'entries': res, 'count': len(res)})
 
 
-# END OF CLERK API
+@app.route('/api/clerk/account_accept', methods=['POST'])
+@token_required
+def accept(current_user):
+    data = request.get_json()
+    user = User.query.filter_by(id=data['user_id']).first()
+
+    user.status = bool(1)
+    print user.id
+    db.session.commit()
+
+    return jsonify({'message':'Account Verified!'})
+
+@app.route('/api/clerk/manage_requests', methods=['GET'])
+@token_required
+def manage_requests(current_user):
+
+    if current_user.role_id != '1':
+        return jsonify ({'message':'Cannot perform that function!'})
+
+    visitations = Visitation.query.all()
+    res = []
+
+    for visitation in visitations:
+        user_data = {}
+        user_data['vId'] = visitation.vId
+        user_data['nameP'] = visitation.nameP
+        user_data['date'] = visitation.date
+        user_data['numberOfVisitors'] = visitation.numberOfVisitors
+        user_data['status'] = visitation.status
+        user_data['id'] = visitation.id
+
+        res.append(user_data)
+
+    return jsonify({'status': 'ok', 'entries': res, 'count': len(res)})
+
+
+@app.route('/api/clerk/schedule_accept', methods=['POST'])
+@token_required
+def schedule_accept(current_user):
+
+    data = request.get_json()
+    visitor = Visitation.query.filter_by(id=data['vis_id']).first()
+
+    # print data['user_id']
+    if str(data['response']) == 'yes':
+        visitor.status = 'APPROVED'
+        db.session.commit()
+        new_logs = VisitationLogs(log_vId=data['vis_id'], user_vId=data['userid'])
+        db.session.add(new_logs)
+        db.session.commit()
+        return jsonify({'message':'Scheduled!'})
+
+
+    elif str(data['response']) == 'no':
+        visitor.status = 'DECLINED'
+        db.session.commit()
+        return jsonify({'message':'Schedule Declined!'})
+
+#END OF CLERK API
+
+
+
+#START OF ADMIN API
+
+@app.route('/api/admin/visit_logs', methods=['GET'])
+@token_required
+def visit_logs(current_user):
+
+    if current_user.role_id != '0':
+       return jsonify({'message': 'Cannot perform that function!'})
+
+    visitationlogs = VisitationLogs.query.all()
+    res = []
+
+    for visit_logs in visitationlogs:
+        user_data = {}
+        user_inf = User.query.filter_by(id=visit_logs.user_vId).first()
+        visittime = Visitation.query.filter_by(id=visit_logs.log_vId).first()
+        user_data['fname'] = user_inf.firstname
+        user_data['mname'] = user_inf.middlename
+        user_data['lname'] = user_inf.lastname
+        user_data['inmate'] = visittime.nameP
+        user_data['date'] = visittime.date
+        res.append(user_data)
+
+    return jsonify({'status': 'ok', 'entries': res, 'count': len(res)})
+#END OF ADMIN API
