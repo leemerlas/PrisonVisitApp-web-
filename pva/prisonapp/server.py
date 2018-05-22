7e45a2833b09e69e2d9df0c67bd3724996bd1f5a
LanceSanchez <lancesanchez16@gmail.com>
2018-05-23 02:34:32 +0800

    debugging

--- pva/prisonapp/server.py
+++ pva/prisonapp/server.py
@@ -20,15 +20,15 @@ def login():
         else:
             if check_password_hash(user.password_hash, request.form['password']):
                 session['user'] = user.username
-                session['fname'] = user.firstname
-                session['role'] = user.role_id
+                # session['fname'] = user.firstname
+                # session['role'] = user.role_id
 
-                if session['role'] == '2':
-                    return redirect(url_for('landing_visitor'))
-                elif session['role'] == '1':
-                    return redirect(url_for('landing_clerk'))
-                elif session['role'] == '0':
-                    return redirect(url_for('landing_admin'))
+                # if session['role'] == '2':
+                #     return redirect(url_for('landing_visitor'))
+                # elif session['role'] == '1':
+                #     return redirect(url_for('landing_clerk'))
+                # elif session['role'] == '0':
+                #     return redirect(url_for('manage_ann'))
 
     return render_template("login-final.html")
 
@@ -38,8 +38,8 @@ def logout():
         return redirect(url_for('login'))
     else:
         session.pop('user')
-        session.pop('fname')
-        session.pop('role')
+        # session.pop('fname')
+        # session.pop('role')
         return redirect(url_for('login'))
 
 
@@ -50,8 +50,8 @@ def register():
 
 @server.route('/visitor/landing', methods=['GET'])
 def landing_visitor():
-    if 'user' in session and session['role'] == '2':
-        return render_template("index.html")
+    if 'user' in session:
+        return render_template("visitor_announcements.html")
     else:
         flash('You are not logged in! Please log in below!')
         return render_template('login-final.html')
@@ -68,31 +68,32 @@ def post_comment():
 @server.route('/visitor/schedule', methods=['GET','POST'])
 def schedule_visit():
     if 'user' in session:
-        return render_template('schedule_visitor.html')
+        return render_template('ScheduleOfVisit.html')
     else:
         flash('You are not logged in! Please log in below!')
         return render_template('login-final.html')
 
-@server.route('/clerk/landing')
-def landing_clerk():
-    if 'user' in session:
-        return render_template('clerk-homepage.html')
-    else:
-        flash('You are not logged in! Please log in below!')
-        return render_template('login-final.html')
+# @server.route('/clerk/landing')
+# def landing_clerk():
+#     if 'user' in session:
+#         return render_template('clerk-homepage.html')
+#     else:
+#         flash('You are not logged in! Please log in below!')
+#         return render_template('login-final.html')
 
-@server.route('/admin/landing')
-def landing_admin():
-    if 'user' in session:
-        return render_template('admin-homepage.html')
-    else:
-        flash('You are not logged in! Please log in below!')
-        return render_template('login-final.html')
+
+# @server.route('/admin/landing')
+# def landing_admin():
+#     if 'user' in session:
+#         return render_template('admin-homepage.html')
+#     else:
+#         flash('You are not logged in! Please log in below!')
+#         return render_template('login-final.html')
 
 
 @server.route('/clerk/view_visitors')
 def view_visitor():
-    if 'user' in session and session['role'] == '1':
+    if 'user' in session:
         return render_template('visitordata.html')
     else:
         flash('You are not logged in! Please log in below!')
@@ -100,7 +101,7 @@ def view_visitor():
 		
 @server.route('/admin/view_visitors')
 def view_visitor_admin():
-    if 'user' in session and session['role'] == '0':
+    if 'user' in session:
         return render_template('visitor_data.html')
     else:
         flash('You are not logged in! Please log in below!')
@@ -108,7 +109,7 @@ def view_visitor_admin():
 
 @server.route('/clerk/manage_requests')
 def clerk_managerequest():
-    if 'user' in session and session['role'] == '1':
+    if 'user' in session:
         return render_template('visitationreq.html')
     else:
         flash('Error!')
@@ -116,15 +117,15 @@ def clerk_managerequest():
 
 @server.route('/admin/visit_logs')
 def admin_visitlogs():
-    if 'user' in session and session['role'] == '0':
+    if 'user' in session:
         return render_template('visit_logs.html')
     else:
         flash('Error!')
-        return render_template('landing_admin.html')
+        return render_template('admin-homepage.html')
 
 @server.route('/clerk/view_prisoners')
 def view_prisoner():
-    if 'user' in session and session['role'] == '1':
+    if 'user' in session:
         return render_template('prisonerdata.html')
     else:
         flash('You are not logged in! Please log in below!')
@@ -134,7 +135,7 @@ def view_prisoner():
 
 @server.route('/admin/add_clerk')
 def add_clerk():
-    if 'user' in session and session['role'] == '0':
+    if 'user' in session:
         return render_template('add_clerk.html')
     else:
         flash('You are not logged in! Please log in below!')
@@ -142,7 +143,7 @@ def add_clerk():
 
 @server.route('/admin/add_prisoner')
 def add_prisoner():
-    if 'user' in session and session['role'] == '0':
+    if 'user' in session:
         return render_template('add_prisoner.html')
     else:
         flash('You are not logged in! Please log in below!')
@@ -150,7 +151,7 @@ def add_prisoner():
 
 @server.route('/clerk/record-visitation')
 def record_visitation():
-    if 'user' in session and session['role'] == '1':
+    if 'user' in session:
         return render_template('addunregisteredvisitor.html')
     else:
         flash('You are not logged in! Please log in below!')
@@ -158,7 +159,7 @@ def record_visitation():
 
 @server.route('/admin/manage-announcements')
 def manage_add():
-    if 'user' in session and session['role'] == '0':
+    if 'user' in session:
         return render_template('create_announcements.html')
     else:
         flash('You are not logged in! Please log in below!')
@@ -166,25 +167,50 @@ def manage_add():
 
 @server.route('/admin/view-announcements')
 def manage_ann():
-    if 'user' in session and session['role'] == '0':
+    if 'user' in session:
         return render_template('manage_announcements.html')
+    else:
+        flash('You are not logged in! Please log in below!')
+        return render_template('login-final.html')
+
+@server.route('/admin/view-comments')
+def view_com():
+    if 'user' in session:
+        return render_template('comments.html')
     else:
         flash('You are not logged in! Please log in below!')
         return render_template('login-final.html')
 
 @server.route('/admin/manage-prisoner')
 def manage_prison():
-    if 'user' in session and session['role'] == '0':
+    if 'user' in session:
         return render_template('prisoner_data.html')
     else:
         flash('You are not logged in! Please log in below!')
         return render_template('login-final.html')
 
+@server.route('/clerk/view-announcements')
+def manage_ann_clerk():
+    if 'user' in session:
+        return render_template('ClerkAnnouncements.html')
+    else:
+        flash('You are not logged in! Please log in below!')
+        return render_template('login-final.html')
+
+@server.route('/visitor/pending')
+def visitor_pending():
+    if 'user' in session:
+        return render_template('visitorlanding_pending.html')
+    else:
+        flash('You are not logged in! Please log in below!')
+        return render_template('login-final.html')
+
+
 
 CORS(server)
-server.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:1234@localhost/prisonapp'
-server.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-dc = SQLAlchemy(server)
+# server.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:1234@localhost/prisonapp'
+# server.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# dc = SQLAlchemy(server)
 server.config['USE_SESSION_FOR_NEXT'] = True
 server.config['CORS_HEADERS'] = 'Content-Type'
 server.config['SECRET_KEY'] = 'thisissecret'
